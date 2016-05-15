8fdc3d324b807bce4e5151b1369c64ff82a52905
Luca Massuda <luca.massuda@gmail.com>
2016-05-16 00:03:27 +0200

    spazi

--- src/JSONReadFromFile.java
+++ src/JSONReadFromFile.java
@@ -9,20 +9,11 @@ import org.json.simple.parser.JSONParser;
   
 public class JSONReadFromFile {
  
-    @SuppressWarnings("unchecked")
-    public static void main(String[] args) {
-    	String file="sources5601-5800.json";
-    	String siteurl="www.footaction.com";
-    	String key="5606-shoes";
-    	System.out.println(urlList(file, siteurl, key).toString());
-    }
     public static List<String> urlList(String file, String siteurl, String key){
     	JSONParser parser = new JSONParser();
     	List<String> sites=new ArrayList<String>();
         try {
- 
             Object obj = parser.parse(new FileReader(file));
- 
             JSONObject jsonObject = (JSONObject) obj;
             JSONObject site = (JSONObject)jsonObject.get(siteurl);
             JSONArray links = (JSONArray)site.get(key);
@@ -32,12 +23,9 @@ public class JSONReadFromFile {
                 //System.out.println(iterator.next());
                 sites.add(iterator.next());
             }
-            
- 
         } catch (Exception e) {
             e.printStackTrace();
         }
 		return sites;
-    	
     }
 }
